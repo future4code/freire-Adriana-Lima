c87d8dcb5cb84130661ac04956fb1e659fbf6563
Adriana de Lima <adriana88dryca@gmail.com>
2022-05-09 18:46:32 -0300

    variaveis

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,77 @@
+
+/*
+Exercicio 1
+let a = 10
+let b = 10
+
+console.log(b) 10
+
+b = 5 // alteracao de 10 para 5
+console.log(a, b) 10, 5
+*/
+
+/*
+Exercicio 2
+let a = 10
+let b = 20
+c = a // c = 10
+b = c // b = 10
+a = b // a = 10
+
+console.log(a, b, c) sera impresso 10, 10, 10
+*/
+
+/*
+Exercicio 3
+let HorasTrabalhadas = prompt("Quantas horas você trabalha por dia?")
+let ValorPorDia = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+*/
+
+//Exercicio de escrita de codigo
+
+let nome 
+console.log(typeof nome)
+let idade 
+console.log(typeof idade)
+
+//Nao declarei o valor da variavel por isso imprimi um undefined
+
+nome = prompt("qual o seu nome?")
+idade = prompt("qual a sua idade?")
+console.log(nome + " " + idade)
+
+ let carro = "voce tem carro?"
+ let casa = "voce tem casa?"
+ let filmes = "voce gosta de filmes?"
+
+ let rescarro = "nao"
+ let rescasa = "sim"
+ let resfilmes = "sim"
+
+ console.log(carro, rescarro)
+ console.log(casa, rescasa)
+ console.log(filmes, resfilmes)
+
+3
+let a = 10
+let b = 25
+c = a
+a = b
+b = c
+
+console.log("O novo valor de a é", a)
+console.log("O novo valor de b é", b)
+
+//desafio extra
+
+let numero1 = prompt("digite o primeiro numero")
+let numero2 = prompt("digite o segundo numero")
+let numero11 = Number(numero1)
+let numero22 = Number(numero2)
+let soma = (numero11 + numero22)
+let somaMultiplicacao = Number(numero1 * numero2)
+
+console.log("o primeiro numero somado ao segundo numero", soma)
+console.log("o primeiro numero multiplicado ao segundo numero", somaMultiplicacao)
+
