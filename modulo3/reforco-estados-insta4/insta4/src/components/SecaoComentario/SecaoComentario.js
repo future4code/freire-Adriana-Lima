7e96faaec69187c861fded59e679dce8b828c0a7
Adriana de Lima <adriana88dryca@gmail.com>
2022-07-27 18:15:39 -0300

    Exercício do Insta4

---
+++ modulo3/reforco-estados-insta4/insta4/src/components/SecaoComentario/SecaoComentario.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react'
+import styled from 'styled-components'
+
+const CommentContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 5px;
+`
+
+const InputComentario = styled.input`
+    width: 100%;
+    margin-right: 5px;
+`
+
+export function SecaoComentario (props) {
+	const [inputComentario, setInputComentario] = useState()
+
+
+	const onChangeComentario = (event) => {
+		setInputComentario(event.target.value)
+
+	}
+
+		return <CommentContainer>
+			<InputComentario
+				placeholder={'Comentário'}
+				value={inputComentario}
+				onChange={onChangeComentario}
+			/>
+			<button onClick={props.aoEnviar}>Enviar</button>
+		</CommentContainer>
+	
+}
+
+export default SecaoComentario;
