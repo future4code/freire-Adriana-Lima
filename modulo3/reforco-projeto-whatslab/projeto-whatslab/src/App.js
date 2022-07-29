9cb9b0363b6ccf5d17735b674734d2c63016abe0
Adriana de Lima <adriana88dryca@gmail.com>
2022-07-29 18:04:38 -0300

    Inicio do Projeto

---
+++ modulo3/reforco-projeto-whatslab/projeto-whatslab/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+import FormInput from './Form/FormInput';
+
+function App() {
+  return (
+    <div>
+      <FormInput/>
+    </div>
+  );
+}
+
+export default App;
